32492808bfec69d1cf94ce9560510a50f6998ad0
chenhui <2631136121@qq.com>
2024-01-14 10:55:09 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui'
 
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
+
+//配置axios全局使用
+axios.defaults.baseURL = 'http://localhost:8081/'
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../Login/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'login',
+    component: Login,
+    meta: {
+        title: '登录'
+    }
+  },
+  {
+    path: '/home',
     name: 'home',
     component: HomeView
   },
@@ -17,11 +26,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
 ]
 
 const router = new VueRouter({
   routes
 })
+
 
 export default router
