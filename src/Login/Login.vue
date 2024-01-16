fc48e1be45b8329fb7f330fc0e55e9ddae9ea77b
chenhui <2631136121@qq.com>
2024-01-16 09:27:45 +0800

    个人中心

--- src/Login/Login.vue
+++ src/Login/Login.vue
@@ -1,33 +1,35 @@
 <template>
   <div class="container">
-    <div class="login-bar"></div>
+    <div class="login-bar">
+      <div class="box">
+        <h1>
+          <img src="../assets/logo.png" class="logo">
+          <span>小阅淘</span>
+        </h1>          
+      </div>
+      <div class="login-tip">你好，欢迎登录</div>
+    </div>
     <div class="login-box">
-      <img src="../assets/welcome.jpg" class="left" width="600px" >
+      <img src="../assets/welcome.jpg" class="left" >
       <div class="login-form">
-        <div class="title">
+        <div class="title" >
           <strong>用户登录</strong>
           <div class="hr"></div>
         </div>
-        <div class="form1"> 
-          <el-input class="username" placeholder="用户名" v-model="username">
-          </el-input>
-        </div>
-        <div class="form2">  
-          <el-input class="password" type="password" suffix-icon="el-icon-lock" placeholder="密码"  v-model="password">
-          </el-input>
-        </div>
-        <div class="login-btn">登录</div>
-
-  
+        <el-input class="username" placeholder="用户账号" v-model="username">
+          <i slot="prefix" class="el-input__icon1 el-icon-user"></i>
+        </el-input>
+        <el-input class="password" type="password"  placeholder="密码"  v-model="password">
+          <i slot="prefix" class="el-input__icon2 el-icon-lock"></i>
+        </el-input>
+        <div class="login-btn" @click="login">登录</div>
+        <div class="register" @click="register">没有账号？去注册</div>
       </div>
     </div>
-    
-    
-
   </div>    
 </template>
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 
 export default {
   data() {
@@ -38,6 +40,15 @@ export default {
     }
   },
   methods:{
+    login(){
+
+    },
+    register(){
+      //点击去注册按钮，路由带参跳转
+      this.$router.push({
+        name:'register',
+      })
+    }
     
   }
 }
@@ -50,38 +61,60 @@ export default {
  font-size:30px;
 }
 .login-bar{
-  background-color: #e83f3f;
+  background-color:#be1313;
   width: 100%;
-  height: 65px;
+  height: 50px;
   padding: 5px 0px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  flex-direction: row;
+}
+.box{
+        width: 200px;
+        height: 50px;
+        align-items: center;
+        position: absolute;
+        left: 8px;
+}
+h1{
+        display: flex;
+        align-items: center;
+        font: normal 28px "华文彩云",Cookie, Arial, Helvetica, sans-serif;
+        padding: 0px 20px;
+}
+.logo{
+        width: 55px;
+        height: 45px;
+}
+.login-tip{
+    font-size: 24px;
+    font-weight: 500;
+    color: #333;
+    font-family: Source Han Sans CN;
+    position:absolute;
+    right:35px ;
+    top: 40px;
 }
 .login-box{
   background:rgb(252, 245, 245);
   width:100%;
-  height:465px;
+  height:500px;
   padding:65px 0px;
   display: -webkit-box;
   display: -ms-flexbox;
   display: flex;
-  -ms-flex-wrap: nowrap;
-  flex-wrap: nowrap;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
   align-items: center;
-  -webkit-box-orient: horizontal;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: row;
   flex-direction: row;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  justify-content: center;
+  box-shadow: 5px 5px 10px rgba(241, 134, 134, 0.5);
 }
 .left{
   position: absolute;
   left:150px;
   border-radius: 4px;
-  /* width:450px; */
   height:388.88px ;
+  width:600px;
+  box-shadow: 5px 5px 8px rgba(241, 134, 134, 0.5);
 }
 .login-form{
   position: absolute;
@@ -89,22 +122,11 @@ export default {
   width: 500px;
   height: 388.88px;
   right: 150px;
-  display: -webkit-box;
-  display: -ms-flexbox;
   display: flex;
-  -ms-flex-wrap: nowrap;
-  flex-wrap: nowrap;
-  -webkit-box-align: center;
-  -ms-flex-align: center;
   align-items: center;
-  -webkit-box-orient: horizontal;
-  -webkit-box-direction: normal;
-  -ms-flex-direction: column;
   flex-direction: column;
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  justify-content: center;
   border-radius: 4px;
+  box-shadow: 5px 5px 8px rgba(241, 134, 134, 0.5);
 }
 .hr{
   width:100%;
@@ -119,43 +141,43 @@ export default {
   top:40px;
   width: 100%;
   height: 45px;
+  text-align:center;
 }
-.form1{
-  background: #fff;
-  position: absolute;
-  top:125px;
-  width: 100%;
-  height: 45px;
-  margin: 20px;
+.el-input__icon1{
+ position: absolute;
+ left:110px;
+ top:163px;
 }
-.form2{
-  background: #fff;
-  position: absolute;
-  top:210px;
-  width: 100%;
-  height: 45px;
-  margin: 20px;
+.el-input__icon2{
+ position: absolute;
+ left:110px;
+ top:228px;
 }
 .login-btn{
     padding-top: 5px;
-    width: 315px;
+    width: 310px;
     height:40px;
     color:#fff;
-    font-size: 15px;
+    font-size: 22px;
     text-align: center;
     border-radius: 4px;
     background-color: #c4261d;
     position: absolute;
-    top:310px;
+    top:285px;
+    right: 90px;
 }
 ::v-deep .username .el-input__inner{
   background-color:#fff;
   text-align:center;
+  align-content: center;
   border-color: #f1f2f5;
   color:#060606;
   width:310px;
   height: 40px;
   border-radius: 4px;
+  position: absolute;
+  right: 88px;
+  top:150px;
 }
 ::v-deep .password .el-input__inner{
   background-color:#fff;
@@ -165,6 +187,16 @@ export default {
   width:310px;
   height: 40px;
   border-radius: 4px;
+  position: absolute;
+  right: 88px;
+  top:215px;
+}
+.register{
+    font-size: 20px;
+    color: #e10c0c;
+    position:absolute;
+    right:33px ;
+    top: 345px;
 }
 
   
---
+++ src/Login/Register.vue
@@ -0,0 +1,221 @@
+<template>
+    <div class="container">
+      <div class="register-bar">
+        <div class="box">
+          <h1>
+            <img src="../assets/logo.png" class="logo">
+            <span>小阅淘</span>
+          </h1>          
+        </div>
+        <div class="register-tip">你好，欢迎注册</div>
+      </div>
+      <div class="register-box">
+        <img src="../assets/welcome.jpg" class="left">
+        <div class="register-form">
+          <div class="title" >
+            <strong>用户注册</strong>
+            <div class="hr"></div>
+          </div>
+          <el-input class="username" placeholder="用户账号" v-model="username">
+            <i slot="prefix" class="el-input__icon1 el-icon-user"></i>
+          </el-input>
+          <el-input class="phone_number" placeholder="手机号"  v-model="phone_number">
+            <i slot="prefix" class="el-input__icon3 el-icon-phone-outline"></i>
+          </el-input>
+          <el-input class="password" type="password"  placeholder="密码"  v-model="password">
+            <i slot="prefix" class="el-input__icon2 el-icon-lock"></i>
+          </el-input>
+          <div class="register-btn" @click="register">注册</div>
+          <div class="login" @click="login">已有账号？去登录</div>
+        </div>
+      </div>
+    </div>    
+  </template>
+  <script>
+  // import axios from 'axios'
+  
+  export default {
+    data() {
+      return {
+       username:"",
+       password:"",
+       
+      }
+    },
+    methods:{
+      register(){
+
+      },
+      login(){
+        this.$router.push({
+            name:'login'
+        })
+      }
+    }
+  }
+  
+  </script>
+  <style scoped>
+  .container{
+   background-color:#ffffff;
+   padding: 20px 20px 20px 20px;
+   font-size:30px;
+  }
+  .register-bar{
+    background-color:#be1313;
+    width: 100%;
+    height: 50px;
+    padding: 5px 0px;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    flex-direction: row;
+  }
+  .box{
+          width: 200px;
+          height: 50px;
+          align-items: center;
+          position: absolute;
+          left: 8px;
+  }
+  h1{
+          display: flex;
+          align-items: center;
+          font: normal 28px "STCaiyun",Cookie, Arial, Helvetica, sans-serif;
+          padding: 0px 20px;
+  }
+  .logo{
+          width: 55px;
+          height: 45px;
+  }
+  .register-tip{
+      font-size: 24px;
+      font-weight: 500;
+      color: #333;
+      font-family: Source Han Sans CN;
+      position:absolute;
+      right:35px ;
+      top: 40px;
+  }
+  .register-box{
+    background:rgb(252, 245, 245);
+    width:100%;
+    height:500px;
+    padding:65px 0px;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    box-shadow: 5px 5px 10px rgba(241, 134, 134, 0.5);
+  }
+  .left{
+    position: absolute;
+    left:150px;
+    border-radius: 4px;
+    height:388.88px ;
+    width:600px;
+    box-shadow: 5px 5px 8px rgba(241, 134, 134, 0.5);
+  }
+  .register-form{
+    position: absolute;
+    background: #fff;
+    width: 500px;
+    height: 388.88px;
+    right: 150px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    border-radius: 4px;
+    box-shadow: 5px 5px 8px rgba(241, 134, 134, 0.5);
+  }
+  .hr{
+    width:100%;
+    border-top: 1px solid #b0b0b0;
+    -webkit-transform: scaleY(.2);
+    transform: scaleY(.2);
+    margin-top: 18px;
+  }
+  .title{
+    background: #fff;
+    position: absolute;
+    top:22px;
+    width: 100%;
+    height: 45px;
+    text-align:center;
+  }
+  .el-input__icon1{
+   position: absolute;
+   left:110px;
+   top:118px;
+  }
+  .el-input__icon2{
+   position: absolute;
+   left:110px;
+   top:228px;
+  }
+  .el-input__icon3{
+   position: absolute;
+   left:110px;
+   top:174px;
+  }
+  .register-btn{
+      padding-top: 5px;
+      width: 310px;
+      height:40px;
+      color:#fff;
+      font-size: 22px;
+      text-align: center;
+      border-radius: 4px;
+      background-color: #c4261d;
+      position: absolute;
+      top:285px;
+      right: 90px;
+  }
+  ::v-deep .username .el-input__inner{
+    background-color:#fff;
+    text-align:center;
+    align-content: center;
+    border-color: #f1f2f5;
+    color:#060606;
+    width:310px;
+    height: 40px;
+    border-radius: 4px;
+    position: absolute;
+    right: 88px;
+    top:105px;
+  }
+  ::v-deep .phone_number .el-input__inner{
+    background-color:#fff;
+    text-align:center;
+    border-color: #f1f2f5;
+    color:#060606;
+    width:310px;
+    height: 40px;
+    border-radius: 4px;
+    position: absolute;
+    right: 88px;
+    top:160px;
+  }
+  ::v-deep .password .el-input__inner{
+    background-color:#fff;
+    text-align:center;
+    border-color: #f1f2f5;
+    color:#060606;
+    width:310px;
+    height: 40px;
+    border-radius: 4px;
+    position: absolute;
+    right: 88px;
+    top:215px;
+  }
+  .login{
+      font-size: 20px;
+      color: #e10c0c;
+      position:absolute;
+      right:33px ;
+      top: 345px;
+  }
+  
+    
+  </style>
